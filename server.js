7b7ea03659b94f1562e30abeb4d5426fbbb77f0d
Pavithr Jain <pavithrjain19@gmail.com>
2025-09-21 17:15:27 +0530

    edited files

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use('/api/auth', authRoutes);
 app.use(errorHandler);
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the Learning Management System API.");
-    console.log("Learning Management System API is running");
+    res.send("Welcome to the Learning Management System API edited.");
+    console.log("Learning Management System API is running edited.");
 });
 
 app.listen(PORT, async () => {
