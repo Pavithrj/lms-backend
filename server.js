02209449fd52d088a1ddc5809f42245788cfa0d3
Pavithr Jain <pavithrjain19@gmail.com>
2025-10-02 16:32:42 +0530

    changed

--- server.js
+++ server.js
@@ -3,26 +3,26 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
 const errorHandler = require('./middleware/error');
-const PORT = process.env.PORT || 5000;
-
 const uiRoutes = require('./routes/uiRoutes');
 const path = require('path');
+
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use("/api/auth", authRoutes);
 
 app.use(errorHandler);
 
-app.use('/', uiRoutes);
+app.use("/", uiRoutes);
 
 app.listen(PORT, async () => {
     await connectDB();
