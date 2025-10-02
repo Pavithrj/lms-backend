7ea17b55c1fda56c165c8fec93fd0358b7cb8245
Pavithr Jain <pavithrjain19@gmail.com>
2025-10-02 16:31:41 +0530

    changed

--- routes/uiRoutes.js
+++ routes/uiRoutes.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('home');
+router.get("/", (req, res) => {
+    res.render("home");
 });
 
-router.get('/signup', (req, res) => {
-    res.render('signup');
+router.get("/signup", (req, res) => {
+    res.render("signup");
 });
 
-router.get('/login', (req, res) => {
-    res.render('login');
+router.get("/login", (req, res) => {
+    res.render("login");
 });
 
-router.get('/forgot-password', (req, res) => {
-    res.render('forgotPassword');
+router.get("/forgot-password", (req, res) => {
+    res.render("forgotPassword");
 });
 
 module.exports = router;
