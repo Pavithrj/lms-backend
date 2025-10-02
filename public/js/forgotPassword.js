b86ab6bbe684158da4a39446d52ece8288a47507
Pavithr Jain <pavithrjain19@gmail.com>
2025-10-02 21:57:04 +0530

    updated

--- public/js/forgotPassword.js
+++ public/js/forgotPassword.js
@@ -13,7 +13,7 @@ document.getElementById("forgotForm").addEventListener("submit", async (e) => {
     }
 
     try {
-        const res = await fetch("/api/auth/forgot-password", {
+        const res = await fetch("https://lms-backend-4f4n.onrender.com/api/auth/forgot-password", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ email })
