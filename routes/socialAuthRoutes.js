8f83b24d114ecc9f3cddeb15e574e447d74c597f
Pavithr Jain <pavithrjain19@gmail.com>
2025-12-20 08:49:05 +0530

    updated

--- routes/socialAuthRoutes.js
+++ routes/socialAuthRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { createGoogleLogin, linkedinCallback, getUser } = require('../controllers/socialAuthController');
+const { createGoogleLogin, linkedinCallback, getUser, createGithubLogin } = require('../controllers/socialAuthController');
 
 const router = express.Router();
 
 router.post("/google-login", createGoogleLogin);
 
 router.get("/callback", linkedinCallback);
+
+router.post("/github-login", createGithubLogin);
 
 router.get("/get-user", getUser);
 
