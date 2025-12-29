3b594654b4ec629e46b6b6e2bdaca77fde7e7300
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-12-29 07:06:30 +0000

    updated

--- server.js
+++ server.js
@@ -16,10 +16,23 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 const app = express();
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
+const allowedOrigins = process.env.CLIENT_URL.split(",");
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin) return callback(null, true);
+
+            if (allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        credentials: true
+    })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
